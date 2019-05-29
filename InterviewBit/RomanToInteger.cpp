273c2ade044ec58ae14888cfb22ee5c98e42b20a
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-05-29 15:15:31 +0530

    easy

--- InterviewBit/RomanToInteger.cpp
+++ InterviewBit/RomanToInteger.cpp
@@ -1,22 +1,22 @@
 int Solution::romanToInt(string A) {
-    unordered_map<char, int> mp;
-    mp['I'] = 1;
-    mp['V'] = 5;
-    mp['X'] = 10;
-    mp['L'] = 50;
-    mp['C'] = 100;
-    mp['D'] = 500;
+    map<char, int> mp;
     mp['M'] = 1000;
-    if(A.size() == 1) 
-        return mp[A[0]];
-    int sum = mp[A[A.length() - 1]];    
-    for(int i = A.length() - 2; i >= 0; i--) {
-        int x = mp[A[i]], y = mp[A[i + 1]];
+    mp['D'] = 500;
+    mp['C'] = 100;
+    mp['L'] = 50;
+    mp['X'] = 10;
+    mp['V'] = 5;
+    mp['I'] = 1;
+    int ans = 0;
+    for(int i = 0; i < A.size() - 1; i++) {
+        int x = mp[A[i]];
+        int y = mp[A[i + 1]];
         if(x < y)
-            sum -= x;
+            ans -= x;
         else
-            sum += x;
+            ans += x;
     }
-    return sum;
+    ans += mp[A.back()];
+    return ans;
 }
 
